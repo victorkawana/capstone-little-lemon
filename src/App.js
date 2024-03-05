9b5816581674eaac764192d4dd1bb90eb31b37a1
Victor <victorkawana10@gmail.com>
2024-03-04 22:38:56 -0300

    04_03_2024

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Header from './Header';
 import Nav from './Nav';
 import Main from './Main';
 import Footer from './Footer';
+import Specials from './Specials';
 import Chicago from './Chicago';
 import {Routes , Route} from  'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <Header />
       <Nav />
       <Main />
+      <Specials />
       <Footer />
-    </>
+    </ChakraProvider>
   );
 }
 
--- src/Specials.js
+++ src/Specials.js
@@ -4,25 +4,33 @@ import "./App.css";
 function Specials() {
     return(
         <>
-            <div className="week-specials-1">
-                <h2>Greek Salad</h2>
-                <p>$12.99</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
+            <div className="week-specials-title">
+                <h3>This Week Specials!</h3>
             </div>
 
-            <div className="week-specials-2">
-                <h2>Greek Salad</h2>
-                <p>$12.99</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
-            </div>
-        
-            <div className="week-specials-3">
-                <h2>Greek Salad</h2>
-                <p>$12.99</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
-            </div>
+            <div className="week-specials">
+                <div className="week-specials-1">
+                    <h3>Greek Salad</h3>
+                    <p>$12.99</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
+                </div>
 
-            <button>Online Menu</button>
+                <div className="week-specials-2">
+                    <h3>Greek Salad</h3> <p>$12.99</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
+                </div>
+            
+                <div className="week-specials-3">
+                    <h3>Greek Salad</h3>
+                    <p>$12.99</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum orci sit amet odio triste.</p>
+                </div>
+            </div>
+            <div>
+                <button>Online Menu</button>
+            </div>
         </>
     )
 }
+
+export default Specials;
